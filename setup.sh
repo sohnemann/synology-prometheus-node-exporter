5bbef74d66a655fe594660206c1befcdd1360a99
Lucas <lucas.maurice@outlook.com>
2020-02-28 13:28:48 -0500

    Updated setup script.

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+DOCKER_VOLUME="/volume1/docker"
+PROMETHEUS_ROOT="${DOCKER_VOLUME}/prometheus"
+
+[ ! -d ${PROMETHEUS_ROOT} ] && mkdir -p ${PROMETHEUS_ROOT} \
+&& echo "folder created ${PROMETHEUS_ROOT}"
+
+curl -fsS -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/lucasmaurice/synology-prometheus/master/docker-compose.yml > ${PROMETHEUS_ROOT}/docker-compose.yml
+echo "docker-compose file created ${PROMETHEUS_ROOT}/docker-compose.yml"
+
+docker-compose -f ${PROMETHEUS_ROOT}/docker-compose.yml up -d
+
+echo "
+#############################################
+Node-Exporter http://`hostname`:9100/metrics
+#############################################
+"
