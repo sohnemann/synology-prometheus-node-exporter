4bf6bc639123fd9aff600b8a59c1f2aadac84683
Leonidas Loucas <merc1031@gmail.com>
2021-09-16 21:38:36 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ PROMETHEUS_ROOT="${DOCKER_VOLUME}/prometheus"
 [ ! -d ${PROMETHEUS_ROOT} ] && mkdir -p ${PROMETHEUS_ROOT} \
 && echo "folder created ${PROMETHEUS_ROOT}"
 
-curl -fsS -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/merc1031/synology-prometheus/master/docker-compose.yml > ${PROMETHEUS_ROOT}/docker-compose.yml
+curl -fsS -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/merc1031/synology-prometheus-node-exporter/master/docker-compose.yml > ${PROMETHEUS_ROOT}/docker-compose.yml
 echo "docker-compose file created ${PROMETHEUS_ROOT}/docker-compose.yml"
 
 docker-compose -f ${PROMETHEUS_ROOT}/docker-compose.yml up -d
